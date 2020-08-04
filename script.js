474cf83eef7894437b7e2fad202742277baeb7c9
Ifeoma Ezekwe <65990265+inezekwe@users.noreply.github.com>
2020-08-04 17:30:32 -0500

    Updated food type filter

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
 var submitBtn = document.getElementById('submitBtn');
 
+var resultsContainer = document.getElementById('resultsContainer');
+
+var priceFilter = document.getElementById('priceFilter');
+
+var foodTypeFilter = document.getElementById('foodTypeFilter');
+
+
+let map;
+let service;
+let infowindow;
+
 
                             //This is the function that retrieves
                             //the location id of the city the 
                             //user types in.
 function locationIdFunc() {
     submitBtn.addEventListener('click', function() {
-
         event.preventDefault();
-        document.getElementById('resultsContainer').innerHTML = '';
-
         var searchBar = document.getElementById('searchBar').value;
         var settings = {
             "async": true,
             "crossDomain": true,
-            "url": `https://tripadvisor1.p.rapidapi.com/locations/search?location_id=1&limit=10&sort=relevance&offset=0&lang=en_US&currency=USD&units=km&query=${searchBar}`,
+            "url": `https://tripadvisor1.p.rapidapi.com/locations/search?location_id=1&limit=30&sort=relevance&offset=0&lang=en_US&currency=USD&units=km&query=${searchBar}`,
             "method": "GET",
             "headers": {
                 "x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
-                "x-rapidapi-key": "b920e36c1emsh38fc765f3e3c7dbp10bccfjsne193d091d660"
+                "x-rapidapi-key": "5f545a0c82mshb2c03dc9e417f9bp1cfbdejsn37162ac5cefa"
             }
         }
-        
         $.ajax(settings).done(function (response) {
             locationId = response.data[0].result_object.location_id;
             console.log(locationId);
             restaurantList(locationId);
+            
         });
-    })
+    });
 }
-
 
                             //This function is taking the locationId
                             //and returning restaurant data 
@@ -37,44 +44,177 @@ function restaurantList(locationId) {
     var settings = {
         "async": true,
         "crossDomain": true,
-        "url": `https://tripadvisor1.p.rapidapi.com/restaurants/list?restaurant_tagcategory_standalone=10591&lunit=mi&restaurant_tagcategory=10591&limit=7&currency=USD&lang=en_US&location_id=${locationId}`,
+        "url": `https://tripadvisor1.p.rapidapi.com/restaurants/list?restaurant_tagcategory_standalone=10591&lunit=mi&restaurant_tagcategory=10591&limit=10&currency=USD&lang=en_US&location_id=${locationId}`,
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
-            "x-rapidapi-key": "b920e36c1emsh38fc765f3e3c7dbp10bccfjsne193d091d660"
+            "x-rapidapi-key": "5f545a0c82mshb2c03dc9e417f9bp1cfbdejsn37162ac5cefa"
         }
     } 
      $.ajax(settings).done(function (response) {
-        console.log(response.data);
-
-
-        var resultsContainer = document.getElementById('resultsContainer');
-        console.log(response.data[0]);
         
-        for(var i=0; i<response.data.length; i++) {
-            if(response.data[i].name == undefined) {
-                response.data.pop();
-            } else {
-                var restaurantNames = `
-                    <div class="card mb-3 justify-content-center" style="max-width: 540px;">
-                        <div class="row no-gutters">
-                            <div class="col-md-4">
-                                <img src="${response.data[i].photo.images.medium.url}" class="card-img" alt="Restaurant Imgage">
-                            </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <h5 class="card-title">${response.data[i].name}</h5>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                `;
-                resultsContainer.innerHTML += restaurantNames;
-            }
-                
+        console.log(response.data);
+        
             
-    }
+            resultsContainer.innerHTML = "";
+            
+            for(var i=0; i<response.data.length; i++) {
+                if(response.data[i].name == undefined) {
+                    response.data.pop();
+                } 
+                else if(priceFilter.value != "Price") {
+                    var restaurantItem = filterPrice(response.data[i]);
+                    if(restaurantItem){
+                        resultsContainer.appendChild(restaurantItem);
+                    }
+                }
+                else if(foodTypeFilter.value != "Food Type") {
+                    var restaurantItem = filterCuisine(response.data[i]);
+                    if(restaurantItem){
+                        resultsContainer.appendChild(restaurantItem);
+                    }
+                }
+                else {
+                    
+                    var restaurantItem = printCard(response.data[i]);
+                    resultsContainer.appendChild(restaurantItem);
+
+                }
+                    
+                
+        }
+        
+        
+      
     });
+
 }
 
-locationIdFunc();
+/*Initializes Google map for the restaurant location when button is clicked*/
+function initMap(coords, response) {
+    
+    infowindow = new google.maps.InfoWindow();
+    
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: coords,
+        zoom: 14,
+        gestureHandling: 'greedy'
+      });
+
+    var request = {
+        query: response,
+        fields: ["name", "formatted_address", "geometry"]
+      };
+      service = new google.maps.places.PlacesService(map);
+      service.findPlaceFromQuery(request, (results, status) => {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (let i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+          }
+          map.setCenter(results[0].geometry.location);
+        }
+      });
+    
+    
+}
+
+/*Creates marker for the Google map and places it on restaurant locationj */
+function createMarker(place) {
+    const marker = new google.maps.Marker({
+      map,
+      position: place.geometry.location
+    });
+    google.maps.event.addListener(marker, "click", () => {
+      infowindow.setContent("<h3>"+ place.name + "</h3>" + "<p>" + place.formatted_address + "</p>");
+      infowindow.open(map);
+    });
+  }
+
+/* Filters the restaurants listings by price level string("$$$") using 
+   the price filter dropdown menu*/
+function filterPrice(response) {
+    let card = null;
+    if(priceFilter.value == response.price_level) {
+        card = printCard(response);
+    }
+    
+    return card;
+    
+    
+}
+
+function filterCuisine(response) {
+    let card = null;
+    for(let i = 0; i < response.cuisine.length; i++) {
+        if(foodTypeFilter.value == response.cuisine[i].name) {
+            card = printCard(response);
+        }
+    }
+
+    return card;
+}
+
+/*Creates card containing all info about each restaurant */
+function printCard(response) {
+    var cardElement = document.createElement('div');
+    cardElement.className = "card mb-3 justify-content-center";
+    cardElement.style.maxWidth = "540px";
+
+    var row = document.createElement('div');
+    row.className = "row no-gutters";
+
+    var pic = document.createElement('div');
+    pic.className = "col-md-4";
+
+    var image = `<img src="${response.photo.images.medium.url}" class="card-img" alt="Restaurant Image">`;
+
+    var info = document.createElement('div');
+    info.className = "col-md-8";
+
+    var cardBody = document.createElement('div');
+    cardBody.className = "card-body";
+    
+    cardInfo = `<h5>${response.name}</h5>
+                <h6>${response.address}</h6>
+                <h6>Phone: ${response.phone}</h6>
+                <p>${response.description}</h6>
+                <p>Price: ${response.price_level} </p>
+                <p>Rating: ${response.rating} </p>
+                `;
+
+    var mapButton = document.createElement('button');
+    mapButton.className = "btn"
+    mapButton.id = response.latitude;
+    mapButton.name = response.longitude;
+    mapButton.style.backgroundColor = "rgb(216,56,38)";
+    mapButton.innerHTML = "Show on Map";
+
+    mapButton.addEventListener('click', function() {
+        var latitude = parseFloat(this.id);
+        var longitude = parseFloat(this.name);
+        var name = this.parentNode.firstChild.innerHTML;
+        console.log(this.parentNode.firstChild.innerHTML);
+
+        var latlong = { lat: latitude, lng: longitude };
+
+        initMap(latlong, name);
+
+    })
+
+    cardBody.insertAdjacentHTML("beforeend", cardInfo); 
+    cardBody.appendChild(mapButton);
+    info.appendChild(cardBody);
+
+    pic.insertAdjacentHTML("beforeend", image);
+
+    row.appendChild(pic);
+    row.appendChild(info);
+
+    cardElement.appendChild(row);
+
+    return cardElement;
+
+}
+
+
+
